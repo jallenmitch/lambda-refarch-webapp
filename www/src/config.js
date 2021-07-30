88b894173b8da7afe23d642b89a28868f08a99bb
Jeff Mitchell <jmitch@vt.edu>
2021-07-29 22:13:05 -0400

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "3qqba9dgtm41d9c0joohosg59d",     // CognitoClientID
+  "api_base_url": "https://f62860g5l9.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-jmitch-todo-app.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d13fuowc8wlkvk.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
